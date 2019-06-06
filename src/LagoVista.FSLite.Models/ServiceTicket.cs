e21c59c2d2ad7f484a929797c0accdb94fe67535
Kevin D. Wolf <kevinw@software-logistics.com>
2019-06-06 08:16:06 -0400

    updates to field on service ticket

--- src/LagoVista.FSLite.Models/ServiceTicket.cs
+++ src/LagoVista.FSLite.Models/ServiceTicket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LagoVista.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,35 @@ namespace LagoVista.FSLite.Models
 {
     public class ServiceTicket : FSModelBase
     {
+        public string TicketId { get; set; }
+
+        public EntityHeader AssignedToId { get; set; }
+
+        public string Subject { get; set; }
+
+        public string DueDate { get; set; }
+
+        public string Status { get; set; }
+        public string StatusDate { get; set; }
+
+        public List<ServiceTicketNote> Notes { get; set; }
+        public List<ServiceTicketStatusHistory> History { get; set; }
+    }
+
+    public class ServiceTicketStatusHistory
+    {
+        public string DateStamp { get; set; }
+        public EntityHeader ChangedBy { get; set; }
+        public string Status { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+    public class ServiceTicketNote
+    {
+        public EntityHeader AddedBy { get; set; }
+        public string DateStamp { get; set; }
+
+        public string Note { get; set; }
     }
 }
